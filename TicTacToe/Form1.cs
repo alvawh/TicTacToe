3a776be31f87504f4839affc42e03c17704f1059
Alva Westrin Haglund <alva.wh@outlook.com>
2023-10-11 10:57:44 +0200

    .

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -15,7 +15,7 @@ namespace TicTacToe
 
         private void displayTurn_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void playerscore2_Click(object sender, EventArgs e)
@@ -28,7 +28,7 @@ namespace TicTacToe
 
         }
         private void button_Click(object sender, EventArgs e)
-        {
+        {  //game starts with x and then switches to o
             Button b = (Button)sender;
             if (turn)
                 b.Text = "X";
@@ -54,7 +54,24 @@ namespace TicTacToe
            else if ((c1.Text == c2.Text) && (c2.Text == c3.Text) && (!c1.Enabled))
                 isWinner = true;
 
-            if (isWinner)
+            // vertical checks for winner
+            if ((a1.Text == b1.Text) && (b1.Text == c1.Text) && (!a1.Enabled))
+                isWinner = true;
+
+            else if ((a2.Text == b2.Text) && (b2.Text == c2.Text) && (!a2.Enabled))
+                isWinner = true;
+
+            else if ((a3.Text == b3.Text) && (b3.Text == c3.Text) && (!a3.Enabled))
+                isWinner = true;
+            // diagonal checks for winner
+            if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && (!a1.Enabled))
+                isWinner = true;
+
+            else if ((c1.Text == b2.Text) && (b2.Text == a3.Text) && (!c1.Enabled))
+                isWinner = true;
+
+            //if there is a winner show message box
+            if (isWinner) 
             {
                 disableButtons();
                 string winner = "";
@@ -65,17 +82,27 @@ namespace TicTacToe
                 MessageBox.Show(winner + " vann!");
             }
         }
-        private void disableButtons()
+        private void disableButtons()//disable buttons after the game is over
         {
             try
-            {
+            { 
                 foreach (Control c in Controls)
                 {
-                    Button b = (Button)c;
-                    b.Enabled = false;
+                   
+                    Button b = c as Button;
+
+                    
+                    if (b != null)
+                    {
+                        
+                        b.Enabled = false;
+                    }
                 }
             }
-            catch { }
+            catch 
+            {
+    
+            }
         }
     }  
 }
